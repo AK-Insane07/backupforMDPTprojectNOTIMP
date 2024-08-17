94b44380179036e219cee0be75322f655e4e7920
AK-Insane07 <127690957+AK-Insane07@users.noreply.github.com>
2024-08-17 19:29:50 +0530

    added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'edu-vic': ['"Edu VIC WA NT Beginner"', 'cursive'],
+        'caveat': ['"Caveat"', 'cursive'],
+        'open-sans':['"Open sans"','"cursive"']
+      },
+    },
+  },
+  plugins: [],
+}
+
